38490b24b768886dd11c4e3a83f6d94a71902243
nabilabedin78-ctrl <nabilabedin78@gmail.com>
2026-09-13 16:14:20 +0600

    Run program by Switch statement

---
+++ Switch.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int choice=1;
+switch(choice){
+case 1:
+printf("you ordered Coffee");
+break;
+case 2:
+printf("you ordered tea");
+break;
+default:
+printf("Invalid Choice");
+}
+return 0;
+}
